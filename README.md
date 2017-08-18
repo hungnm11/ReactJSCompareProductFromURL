5a1afd65e402522b02239915834ad0812c909af9
Hung Nguyen <hung.nguyen@sentifi.com>
2017-08-19 06:25:16 +0700

    init new project

---
+++ README.md
@@ -0,0 +1,9 @@
+#### Familiar with Git?
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> git clone https://github.com/hungnm11/ReactJSCompareProductFromURL.git
+> cd ReduxSimpleStarter
+> npm install
+> npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    },
+    {
+      test: /\.less$/,
+      loader: 'style!css!less'
+    },
+
+    // SASS
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }
+  ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', 'css']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};

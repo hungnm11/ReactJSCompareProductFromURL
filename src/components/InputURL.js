c1bc070afc771bf2633cc5783c2147a2c72ab0f9
Hung Nguyen <hung.nguyen@sentifi.com>
2017-08-19 09:30:59 +0700

    add redux logger

--- src/components/InputURL.js
+++ src/components/InputURL.js
@@ -3,28 +3,62 @@ import { Field, reduxForm } from 'redux-form';
 
 class InputURL extends Component {
 
-  renderField(field) {
-    return (
-      <div>
-        {field.label}
-        <input
-          type={field.type}
-          placeholder={field.placeholder}
-        />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+
+    this.state = {
+      url1: '',
+      url2: ''
+    };
+  }
+
+  // renderField(field) {
+  //   return (
+  //     <div>
+  //       {field.label}
+  //       <input
+  //         type={field.type}
+  //         placeholder={field.placeholder}
+  //         onChange={field.input.onChange}
+  //       />
+  //     </div>
+  //   );
+  // }
+
+  onInputChange(event) {
+    console.log(event);
+
+  }
+
+  onFormSubmit(event) {
+    console.log('Event', event);
   }
 
   render() {
+    const { handleSubmit } = this.props;
+    console.log(this.props);
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onFormSubmit.bind(this))}>
         <Field
           label="URL 1"
           name="url1"
           type="text"
           placeholder="URL 1"
-          component={this.renderField}
+          component="input"
+          onChange={this.onInputChange}
         />
+
+        <Field
+          label="URL 2"
+          name="url2"
+          type="text"
+          placeholder="URL 2"
+          component="input"
+          onChange={this.onInputChange}
+        />
+
+        <button type="submit">Submit</button>
       </form>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './components/app';
 import reducers from './reducers';
 import '../style/style.scss';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk, logger)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
